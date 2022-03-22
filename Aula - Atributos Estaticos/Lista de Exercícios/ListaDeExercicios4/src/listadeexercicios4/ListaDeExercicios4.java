054faad16f2941ed85c488d6a95e83d6ff20d3f0
pQuintans <caixote.dom@gmail.com>
2022-03-22 13:36:35 -0300

    Debug da atividade 4 feita em aula sobre Atributos Estáticos

--- Aula - Atributos Estaticos/Lista de Exercícios/ListaDeExercicios4/src/listadeexercicios4/ListaDeExercicios4.java
+++ Aula - Atributos Estaticos/Lista de Exercícios/ListaDeExercicios4/src/listadeexercicios4/ListaDeExercicios4.java
@@ -1,4 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package listadeexercicios4;
+
+/**
+ *
+ * @author aluno
+ */
 public class ListaDeExercicios4 {
+
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
         SimuladorCaixaBanco caixa1 = new SimuladorCaixaBanco(1);
         SimuladorCaixaBanco caixa2 = new SimuladorCaixaBanco(2);
@@ -14,4 +29,5 @@ public class ListaDeExercicios4 {
         caixa2.proximoAtendimento();
         caixa1.proximoAtendimento();
     }
+    
 }
--- Aula - Atributos Estaticos/Lista de Exercícios/ListaDeExercicios4/src/listadeexercicios4/SimuladorCaixaBanco.java
+++ Aula - Atributos Estaticos/Lista de Exercícios/ListaDeExercicios4/src/listadeexercicios4/SimuladorCaixaBanco.java
@@ -1,3 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package listadeexercicios4;
+
+/**
+ *
+ * @author aluno
+ */
 public class SimuladorCaixaBanco {
     private static int numeroDoCliente = 0;
     private int numeroDoCaixa;
