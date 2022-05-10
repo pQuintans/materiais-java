33a4a4e10e0df4775f34fb09e498704e5d032338
pQuintans <caixote.dom@gmail.com>
2022-05-10 14:44:21 -0300

    Começo Exemplo sobre MVC

---
+++ MVC/ExemploMVC/src/controller/AlunoController.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import java.util.ArrayList;
+import model.Aluno;
+
+/**
+ *
+ * @author aluno
+ */
+public class AlunoController {
+    private ArrayList<Aluno> lista;
+    
+    public AlunoController(){
+        lista = new ArrayList();
+    }
+    
+    public void cadastrar(String nome, int idade) {
+        Aluno al = new Aluno(nome, idade);
+        lista.add(al);
+    }
+
+    public ArrayList<Aluno> getLista() {
+        return lista;
+    }
+
+    public void setLista(ArrayList<Aluno> lista) {
+        this.lista = lista;
+    }
+}
